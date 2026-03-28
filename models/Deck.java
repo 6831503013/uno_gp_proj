2ec01ff4cd56095ec780ca2d9fde37a5115518b1
Kenny <6831503010@lamduan.mfu.ac.th>
2026-03-28 17:52:54 +0700

    feat: add creation of 108 uno cards -two of each number except 0 -two of each special card -4 of each wild card

--- models/Deck.java
+++ models/Deck.java
@@ -1,6 +1,7 @@
 package models;
 
 import java.util.*;
+import data.GameConstants;
 
 public class Deck {
 
@@ -10,12 +11,34 @@ public class Deck {
     public Deck() {
         cards = new ArrayList<>();
         createDeck();
-        shuffleDeck();
-        drawPile = new LinkedList<>(cards);
+        // shuffleDeck();
+        // drawPile = new LinkedList<>(cards);
     }
 
     private void createDeck() {
         // TODO: Create UNO cards
+        for (String color : GameConstants.COLORS) {
+            for (String value : GameConstants.NUMBER_VALUES) {
+                cards.add(new Card(color, value));
+                if (!value.equals("0")) {
+                    cards.add(new Card(color, value)); // Two of each number except 0
+                }
+            }
+
+            // Add special cards
+            for (String value : GameConstants.SPECIAL_VALUES) {
+                cards.add(new Card(color, value));
+                cards.add(new Card(color, value)); // Two of each special card
+            }
+        }
+
+        // Add wild cards
+        for (String value : GameConstants.WILD_VALUES) {
+            cards.add(new Card("Wild", value));
+            cards.add(new Card("Wild", value));
+            cards.add(new Card("Wild", value));
+            cards.add(new Card("Wild", value)); // Wild cards have no color,4 of each wild card
+        }
     }
 
     private void shuffleDeck() {
@@ -29,5 +52,13 @@ public class Deck {
 
     public boolean isEmpty() {
         return drawPile.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+        for (Card card : deck.cards) {
+            System.out.println(card);
+        }
+        System.out.println("Total cards in deck: " + deck.cards.size());
     }
 }
