39a1c92d164bf645a978595fc56693206ee3940e
Myo Satt Kyaw <6831503013@lamduan.mfu.ac.th>
2026-03-28 13:37:59 +0700

    Add constants value for cards

---
+++ data/GameConstants.java
@@ -0,0 +1,24 @@
+package data;
+
+public class GameConstants {
+
+    // Card Colors
+    public static final String[] COLORS = {
+        "Red", "Blue", "Green", "Yellow"
+    };
+
+    // Number Cards
+    public static final String[] NUMBER_VALUES = {
+        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+    };
+
+    // Special Cards
+    public static final String[] SPECIAL_VALUES = {
+        "Skip", "Reverse", "Draw2"
+    };
+
+    // Wild Cards
+    public static final String[] WILD_VALUES = {
+        "Wild", "WildDraw4"
+    };
+}
