4b38f1e9080b2d5a6e738008403797a9ded921b3
Bhone Myat Paing <6831503004@lamduan.mfu.ac.th>
2026-03-29 14:30:33 +0700

    Done

--- controllers/GameRules.java
+++ controllers/GameRules.java
@@ -1,15 +1,55 @@
 package controllers;
 
 import models.Card;
+import models.Player;
+import models.Deck;
 
 public class GameRules {
 
     public static boolean isValidMove(Card playedCard, Card topCard) {
-        // TODO: Match color/value/wild
+
+        //Checks the color, value, and if it's a wild card to determine if the move is valid
+        if (playedCard.getColor().equals(topCard.getColor())
+                || playedCard.getValue().equals(topCard.getValue())
+                || playedCard.getColor().equals("Wild")
+                || playedCard.getValue().equals("WildDraw4")) { 
+            return true;
+        }
         return false;
     }
 
-    public static void applySpecialCard(Card card) {
-        // TODO: Handle Skip, Reverse, Draw2, Wild
+    //Applies the effects of special cards such as Skip, Reverse, Draw2, Wild, and WildDraw4
+    public static void applySpecialCard(Card card,GameController game, Deck deck) {
+
+       
+        switch (card.getValue()) {
+            case "Skip":
+                game.skipNextPlayer();
+                break;
+
+            case "Reverse":
+                game.reverseDirection();
+                break;
+
+            case "Draw2":
+                Player next = game.getNextPlayer();
+                next.addCard(deck.drawCard());
+                next.addCard(deck.drawCard());
+                game.skipNextPlayer();
+                break;
+
+            case "Wild":
+                game.changeColor();
+                break;
+
+            case "WildDraw4":
+                Player nextPlayer = game.getNextPlayer();
+                for (int i = 0; i < 4; i++) {
+                    nextPlayer.addCard(deck.drawCard());
+                }
+                game.skipNextPlayer();
+                game.changeColor();
+                break;
+        }
     }
 }
--- utils/RecursionHelper.java
+++ utils/RecursionHelper.java
@@ -8,6 +8,22 @@ public class RecursionHelper {
 
     public static Card drawUntilPlayable(Player player, Deck deck, Card topCard) {
         // TODO: Recursive logic
-        return null;
+        if (deck.isEmpty())
+        {
+            return null;
+        }
+
+        Card drawnCard = deck.drawCard();
+
+        player.addCard(drawnCard);
+
+        if (drawnCard.getColor().equals(topCard.getColor())
+                || drawnCard.getValue().equals(topCard.getValue())
+                || drawnCard.getColor().equals("Wild")) {
+            return drawnCard;
+        } else {
+            return drawUntilPlayable(player, deck, topCard);
+        }
+
     }
 }
